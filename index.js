b2b4af030070394046df26c3f8ff310aec49f78e
VatsalNilay <vatsalnilay21@gmail.com>
2023-05-02 12:38:59 +0530

    Changed view engine and some refinement

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken');
 const port = 3001
 
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.json()) //accept data in json format
+app.use(express.urlencoded())  //decode the data sent through the form
+
+
+
+//Sasta database
 const USERS = [];
 
 const QUESTIONS = [{
@@ -21,25 +31,19 @@ const SUBMISSION = [
 
 
 
-//Middlewares
-app.use(express.json()) //accept data in json format
-app.use(express.urlencoded())  //decode the data sent through the form
-
-
 //Routes
-// app.get('/', function(req,res){
-//     res.redirect('/signup')
-// });
+app.get('/users',(req,res) =>{
+  res.json(USERS)
+})
 app.get('/signup', function(req,res){
     // res.render("signup1")
-    res.sendFile(__dirname + '/public/signup1.html');
+    res.render('signup1')
 });
 app.post('/signup', function(req, res) {
-    // Add logic to decode body
     // const { email, password } = req.body;
     const email = req.body.email
     const password = req.body.password
-    // console.log(req.body)
+
 
     // Check if user already exists
     const userExists = USERS.find(user => user.email === email);
@@ -49,16 +53,17 @@ app.post('/signup', function(req, res) {
     }
   
     // Store email and password (as is for now) in the USERS array
-    USERS.push({ email, password });
+    USERS.push({ "email" : email, "password": password, "role" : "admin" });
   
-    // Return success response with HTTP 200 status code
+    
     res.status(200).redirect('/login');
+    // res.status(201).send()
   });
-  
+
 
 
 app.get('/login', function(req, res){
-    res.sendFile(__dirname + '/public/login1.html')
+    res.render('login1');
 });
 app.post('/login', function(req, res) {
   // Add logic to decode body
@@ -68,16 +73,12 @@ app.post('/login', function(req, res) {
   // Check if the user with the given email exists in the USERS array
   const userExists1 = USERS.find(user => user.email === email1)
  
-  // Also ensure that the password is the same
-  // If the password is the same, return back 200 status code to the client
-  // Also send back a token (any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client
   if(!userExists1)
     res.status(401).redirect('/login?userExists=false');
   else if(userExists1.password === password1)
   {
-    const token = 'This is your token';
-    res.status(200).json({token});
+    const accessToken = jwt.sign({ email: email1 }, process.env.ACCESS_TOKEN_SECRET);
+    res.status(200).json({accessToken: accessToken});
   }
   else
     res.status(401).redirect('/login?wrongpass=true');   
@@ -85,10 +86,8 @@ app.post('/login', function(req, res) {
 })
 
 app.get('/questions', function(req, res) {
-
-  //return the user all the questions in the QUESTIONS array
-  res.send("Hello World from route 3!")
-})
+  res.render('ques1', { questions: QUESTIONS });
+});
 
 app.get("/submissions", function(req, res) {
    // return the users submissions for this problem
@@ -99,6 +98,7 @@ app.get("/submissions", function(req, res) {
 app.post("/submissions", function(req, res) {
    // let the user submit a problem, randomly accept or reject the solution
    // Store the submission in the SUBMISSION array above
+   
   res.send("Hello World from route 4!")
 });
 
